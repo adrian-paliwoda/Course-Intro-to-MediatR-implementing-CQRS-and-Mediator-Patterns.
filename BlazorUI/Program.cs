0cf6854a8f39cf2a85ae19978e7714fb6cef866e
Adrian Paliwoda <adrian94@tutanota.com>
2025-01-23 12:31:40 +0100

    Registered MediatR

--- BlazorUI/Program.cs
+++ BlazorUI/Program.cs
@@ -1,9 +1,14 @@
 using BlazorUI.Components;
+using DemoLibrary;
+using DemoLibrary.DataAccess;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
+builder.Services
+    .AddSingleton<IDataAccess, DemoDataAccess>()
+    .AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(MediatREntryPoint).Assembly))
+    .AddRazorComponents()
     .AddInteractiveServerComponents();
 
 var app = builder.Build();
@@ -11,7 +16,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
